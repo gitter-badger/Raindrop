b0cc08011a990b5703ebf6c62919e7e3c5f60ea4
Krasno <46050962+KrasnotR@users.noreply.github.com>
2019-01-07 15:53:18 -0200

    Just decided to rewrite everything because im a dumbdumb

--- Raindrop/Kernel.cs
+++ Raindrop/Kernel.cs
@@ -1,5 +1,4 @@
-﻿using Raindrop.Interpreter;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
@@ -8,26 +7,19 @@ namespace Raindrop
 {
     public class Kernel : Sys.Kernel
     {
-        public static List<KThread> Pool      = new List<KThread>();
+        /*public static List<KThread> Pool      = new List<KThread>();
         public static List<Process> Processes = new List<Process>();
 
-        public static KThread currentThread;
+        public static KThread currentThread;*/
 
         protected override void BeforeRun()
         {
             Console.WriteLine("Raindrop OS created by Krasno.");
-            Engine en = new Engine();
-            Process Bootstrap = new Process("");
         }
 
         protected override void Run()
         {
-            // Step through every thread
-            for (var i = 0; i < Pool.Count; i++)
-            {
-                currentThread = Pool[i];
-                Pool[i].Step();
-            }
+
         }
     }
 }
